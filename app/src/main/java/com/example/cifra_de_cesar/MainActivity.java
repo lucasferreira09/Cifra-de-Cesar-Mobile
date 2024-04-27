c39c4bb5a453b2106e8a98d0bdda530b8636a8e6
lucasferreira09 <148561715+lucasferreira09@users.noreply.github.com>
2024-04-27 12:07:28 -0300

    Update:
    ViewBinding
    Implementação de ClipToBoard

--- app/src/main/java/com/example/cifra_de_cesar/MainActivity.java
+++ app/src/main/java/com/example/cifra_de_cesar/MainActivity.java
@@ -5,38 +5,37 @@ import static android.service.autofill.Validators.or;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.cifra_de_cesar.databinding.ActivityMainBinding;
+
 import org.w3c.dom.Text;
 
 import java.util.Arrays;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editChave;
-    private ImageButton cifrar;
-    private Button insiraMsgOriginal;
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        editChave = findViewById(R.id.editChave);
-        cifrar = findViewById(R.id.cifrar);
-        insiraMsgOriginal = findViewById(R.id.insiraMsgOriginal);
-
-        editChave = findViewById(R.id.editChave);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
 
         //Cria o AlertDialog para inserir a mensagem a ser cifrada
@@ -50,12 +49,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
+                //Mostra as três primeiras letras da mensagem
+                //Assim o usuário sabe se sua Msg foi aplicada
+                if (!msgPraCifrar.getText().toString().equals("")) {
+                    binding.editMsgOriginal.setText(
+                            msgPraCifrar.getText().toString().substring(0, 3) + "...");
+                }
             }
         });
         AlertDialog dialog2 = builder2.create();
 
         //Botão para inserir a mensagem a ser cifrada
-        insiraMsgOriginal.setOnClickListener(new View.OnClickListener() {
+        binding.editMsgOriginal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog2.show();
@@ -65,16 +70,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
         //Ao ser acionado, irá cifrar a mensagem
         //e abrir uma caixa de diálogo com a mensagem Cifrada
-        cifrar.setOnClickListener(new View.OnClickListener() {
+        binding.cifrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //Verifica se foi inserido alguma Chave
                 int chave = 0;
-                String chaveText = editChave.getText().toString();
+                String chaveText = binding.editChave.getText().toString();
                 if (!TextUtils.isEmpty(chaveText)) {
                     chave = Integer.parseInt(chaveText);
                 }
@@ -92,18 +96,33 @@ public class MainActivity extends AppCompatActivity {
                     builder.setView(infla);
                     AlertDialog dialog = builder.create();
 
+
                     //Adiciona a mensagem Cifrada no TextView
                     TextView textMensagemCifrada = infla.findViewById(R.id.textResult);
                     textMensagemCifrada.setText(cifrador.cifrarMensagem(msgOriginal));
                     dialog.show();
+
+                    //Botão copia Texto Cifrado
+                    ImageButton buttonCopy = infla.findViewById(R.id.buttonCopy);
+                    buttonCopy.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ClipboardManager clipboardManager = (ClipboardManager)
+                                    getSystemService(Context.CLIPBOARD_SERVICE);
+
+                            ClipData clipData = ClipData.newPlainText("texto_cifrado", textMensagemCifrada.getText().toString());
+                            clipboardManager.setPrimaryClip(clipData);
+                            Toast.makeText(MainActivity.this, "Copiado", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                 } else {
                     Toast.makeText(MainActivity.this, "Dados Inválidos", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
-
     }
+
     public boolean verificaExisteMensagem(String mensagem, int chave) {
 
         if (!mensagem.equals("") && chave <= 24 && chave >= 1) {
@@ -112,5 +131,4 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
-
 }
